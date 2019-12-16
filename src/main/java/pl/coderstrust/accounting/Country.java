cbbf9e4e5eec6b5e7b1c1d6d5b18edaf436b2482
glk <52168046+mgrzelczyk@users.noreply.github.com>
2019-12-16 06:33:11 +0100

    Create new SOAP task

---
+++ src/main/java/pl/coderstrust/accounting/Country.java
@@ -0,0 +1,17 @@
+package pl.coderstrust.accounting;
+
+public class Country {
+
+    public Country() {
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
