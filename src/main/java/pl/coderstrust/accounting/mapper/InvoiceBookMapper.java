cfdbb6352311c7100435a622299a86b0c71d2b16
glk <52168046+mgrzelczyk@users.noreply.github.com>
2019-12-24 07:18:06 +0100

    Update after consultation

--- src/main/java/pl/coderstrust/accounting/mapper/InvoiceBookMapper.java
+++ src/main/java/pl/coderstrust/accounting/mapper/InvoiceBookMapper.java
@@ -7,7 +7,7 @@ public class InvoiceBookMapper {
     public static io.spring.guides.gs_producing_web_service.Invoice toSoapInvoice (pl.coderstrust.accounting.model.Invoice invoice) {
 
         Invoice invoice1 = new Invoice();
-        
+
         // new Invoice web service nowy obiekt
         // invoice set wszystkie pola
         return toSoapInvoice(invoice);
