4e7caf95a4aff3237058afe8dbcef8a553582d98
glk <52168046+mgrzelczyk@users.noreply.github.com>
2020-02-16 21:09:03 +0100

    Prepare methods

--- src/main/java/pl/coderstrust/accounting/services/TaxCalculatorService.java
+++ src/main/java/pl/coderstrust/accounting/services/TaxCalculatorService.java
@@ -1,5 +1,66 @@
 package pl.coderstrust.accounting.services;
 
+import org.springframework.stereotype.Service;
+import pl.coderstrust.accounting.model.Invoice;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+
+@Service
 public class TaxCalculatorService {
-    
+
+    BigDecimal calculateIncome() {
+        return null;
+    }
+
+    BigDecimal calculateCost() {
+        return null;
+    }
+
+    BigDecimal calculateIncomeVat() {
+        return null;
+    }
+
+    BigDecimal calculateOutcomeVat() {
+        return null;
+    }
+
+    private BigDecimal calculateTaxBase() {
+        return null;
+    }
+
+    public Map<String, BigDecimal> taxSummary() {
+        return null;
+    }
+
+    private Predicate<Invoice> getCompanyIdSeller() {
+        return null;
+    }
+
+    private Predicate<Invoice> getCompanyIdBuyer() {
+        return null;
+    }
+
+    private BigDecimal getCostValue() {
+        return null;
+    }
+
+    private BigDecimal getNetValue() {
+        return null;
+    }
+
+    private BigDecimal getVatValue() {
+        return null;
+    }
+
+    private BigDecimal getIncomeVatValue() {
+        return null;
+    }
+
+    private List<Invoice> getInvoiceByDate() {
+        return null;
+    }
+
 }
