b053afd335db28e42b47ce63e758609101be57fd
glk <52168046+mgrzelczyk@users.noreply.github.com>
2020-02-17 21:12:40 +0100

    Prepare methods

--- src/main/java/pl/coderstrust/accounting/services/TaxCalculatorService.java
+++ src/main/java/pl/coderstrust/accounting/services/TaxCalculatorService.java
@@ -1,66 +1,28 @@
 package pl.coderstrust.accounting.services;
 
 import org.springframework.stereotype.Service;
-import pl.coderstrust.accounting.model.Invoice;
+import pl.coderstrust.accounting.model.InvoiceEntry;
 
 import java.math.BigDecimal;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Predicate;
 
 @Service
 public class TaxCalculatorService {
 
-    BigDecimal calculateIncome() {
+    BigDecimal calculateVatValue(InvoiceEntry invoiceEntry) {
+        int vatValue = invoiceEntry.getVatValue();
+        BigDecimal vatInvoiceValue = null;
+        BigDecimal vatBigDecimal = multiply(vatValue, vatInvoiceValue);
+        BigDecimal priceGross = invoiceEntry.getPrice();
+        BigDecimal vatCalculated = vatBigDecimal.multiply(priceGross).multiply(BigDecimal.valueOf(0.01));
+        return vatCalculated;
+    }
+
+    BigDecimal calculateNetPrice(InvoiceEntry invoiceEntry) {
         return null;
     }
 
-    BigDecimal calculateCost() {
-        return null;
-    }
-
-    BigDecimal calculateIncomeVat() {
-        return null;
-    }
-
-    BigDecimal calculateOutcomeVat() {
-        return null;
-    }
-
-    private BigDecimal calculateTaxBase() {
-        return null;
-    }
-
-    public Map<String, BigDecimal> taxSummary() {
-        return null;
-    }
-
-    private Predicate<Invoice> getCompanyIdSeller() {
-        return null;
-    }
-
-    private Predicate<Invoice> getCompanyIdBuyer() {
-        return null;
-    }
-
-    private BigDecimal getCostValue() {
-        return null;
-    }
-
-    private BigDecimal getNetValue() {
-        return null;
-    }
-
-    private BigDecimal getVatValue() {
-        return null;
-    }
-
-    private BigDecimal getIncomeVatValue() {
-        return null;
-    }
-
-    private List<Invoice> getInvoiceByDate() {
-        return null;
+    private static BigDecimal multiply ( int a, BigDecimal b ) {
+        return BigDecimal.valueOf(a).multiply(b);
     }
 
 }
