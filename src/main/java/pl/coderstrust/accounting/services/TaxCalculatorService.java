e647360534d860fdffbda6f5fd1d65e4ca8e4dbe
glk <52168046+mgrzelczyk@users.noreply.github.com>
2020-02-20 20:56:13 +0100

    Update

--- src/main/java/pl/coderstrust/accounting/services/TaxCalculatorService.java
+++ src/main/java/pl/coderstrust/accounting/services/TaxCalculatorService.java
@@ -3,7 +3,6 @@ package pl.coderstrust.accounting.services;
 import org.hibernate.boot.model.relational.Database;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.coderstrust.accounting.model.InvoiceEntry;
 
