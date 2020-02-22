b7c71c3b3bee436d4abb43555adcd9143b5f2d68
glk <52168046+mgrzelczyk@users.noreply.github.com>
2020-02-22 20:53:23 +0100

    Update

--- src/main/java/pl/coderstrust/accounting/services/TaxCalculatorController.java
+++ src/main/java/pl/coderstrust/accounting/services/TaxCalculatorController.java
@@ -2,6 +2,7 @@ package pl.coderstrust.accounting.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,7 @@ public class TaxCalculatorController {
     public TaxCalculatorController(TaxCalculatorService taxService) {
         this.taxService = taxService;
     }
+
+    @GetMapping("/invoices")
 
 }
